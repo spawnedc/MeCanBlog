38422de0c7bf3f34fb3c88b040f3f57aa419cebd
Can Yilmaz <can@potatolondon.com>
2011-10-17 21:23:33 +0100

    More initial setup

---
+++ mecanblog/models.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="spawnedc"
+__date__ ="$Oct 17, 2011 9:15:24 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
